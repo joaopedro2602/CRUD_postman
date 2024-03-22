c279cb716e864e592d29631f7dfcfae9122143ef
João Pedro Cabral <99431017+joaopedro2602@users.noreply.github.com>
2024-03-22 10:28:02 -0300

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'flash/perfomphp',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '86abe24f5fea9ce79d6c416b5b96e5b2a1c54501',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'flash/perfomphp' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '86abe24f5fea9ce79d6c416b5b96e5b2a1c54501',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
